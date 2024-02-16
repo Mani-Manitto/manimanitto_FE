27d036677c0d51893fc374092755f62e39701131
KECO-00 <ksang98@icloud.com>
2024-02-16 20:45:17 +0900

    feat: 기본 CSS 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,27 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container">
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body {
+          margin: 0;
+          padding: 0;
+          background-color: blue;
+          display: flex;
+          justify-content: center;
+        }
+        
+        .container {
+          min-width: 350px;
+          max-width: 450px;
+          width: 100vh;
+          height: 100vh;
+          background-color: white;
+        }
+      `}</style>
+    </div>
+    
+  );
 }

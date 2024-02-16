210e7021427385dd5ba604d649bca093f955f283
KECO-00 <ksang98@icloud.com>
2024-02-16 22:58:07 +0900

    feat: 텍스트 변경 시 애니메이션 적용 ver.1

--- pages/createManito/createMain.tsx
+++ pages/createManito/createMain.tsx
@@ -1,13 +1,34 @@
 import React, {useState, useEffect} from "react";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 export default function CreateHome() {
-    const [greeting, setGreeting] = useState('반가워요, \n마니또지기님!');
+    const [greeting1, setGreeting1] = useState(['반가워요,', <br key="br" />, '마니또지기님!']);
+    const [greeting2, setGreeting2] = useState(['마니또 게임을', <br key="br"/>, '도와드릴게요']);
+    const [displayGreeting, setDisplayGreeting] = useState(true);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setDisplayGreeting(prev => !prev);
+        }, 5000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    const display = displayGreeting ? greeting1 : greeting2;
+    const key = displayGreeting ? "greeting1" : "greeting2";
+
 
   return (
     <>
         <div className="container">
             <p className="logo">마니마니또</p>
-            <h1 className="greeting">{greeting}</h1>
+            <TransitionGroup>
+                <CSSTransition key={key} timeout={500} classNames="fade" mountOnEnter unmountOnExit>
+                    <h1 className="greeting">{display}</h1>
+                </CSSTransition>
+            </TransitionGroup>
 
             <div className="character-container">
                 <iframe src="https://lottie.host/embed/be36fba1-43f0-4d12-a882-cf40fa5d84bd/npTev2uCYy.json"></iframe>
@@ -22,13 +43,48 @@ export default function CreateHome() {
                     justify-content: space-around;
                     align-items: center;
                 }
+                .greeting {
+                    text-align: center;
+                }
+                .fade-enter {
+                    opacity: 0;
+                }
+                .fade-enter-active {
+                    opacity: 1;
+                    transition: opacity 1000ms;
+                }
+                .fade-exit {
+                    opacity: 1;
+                }
+                .fade-exit-active {
+                    opacity: 0;
+                    transition: opacity 1000ms;
+                }
+
                 .character-container {
                     position: relative;
-                    display: inline-block;
+                    width: 300px;
+                    height: 300px;
+                    overflow: hidden;
+                }
+
+                .character-container::before {
+                    content: '';
+                    position: absolute;
+                    width: 200%;
+                    height: 200%;
+                    top: -50%;
+                    left: -50%;
+                    background-color: #bceceb;
+                    border-radius: 50%;
+                    z-index: -1;
                 }
 
                 iframe{
                     border: none;
+                    position: relative;
+                    width: 100%;
+                    height: 100%;
 
                 }
                 

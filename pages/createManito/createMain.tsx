59d523dae26f96feca299b9d5b48c39b2ff5c0e6
KECO-00 <ksang98@icloud.com>
2024-02-23 23:44:51 +0900

    feat: text 애니메이션 css로 수정

--- pages/createManito/createMain.tsx
+++ pages/createManito/createMain.tsx
@@ -1,39 +1,32 @@
 import React, {useState, useEffect} from "react";
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+const sentences = ['반가워요, \n마니또지기님!', '마니또 게임을 \n도와드릴게요'];
 
 export default function CreateHome() {
-    const [greeting1, setGreeting1] = useState(['반가워요,', <br key="br" />, '마니또지기님!']);
-    const [greeting2, setGreeting2] = useState(['마니또 게임을', <br key="br"/>, '도와드릴게요']);
-    const [displayGreeting, setDisplayGreeting] = useState(true);
+    const [index, setIndex] = useState<number>(0);
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            setDisplayGreeting(prev => !prev);
-        }, 5000);
+        const interval = setInterval(() => {
+            setIndex((prev) => (prev === sentences.length - 1 ? 0 : prev + 1));
+        }, 4900);
 
-        return () => {
-            clearInterval(timer);
-        };
-    }, []);
-
-    const display = displayGreeting ? greeting1 : greeting2;
-    const key = displayGreeting ? "greeting1" : "greeting2";
+        return () => clearInterval(interval);
+    },[]);
 
 
   return (
     <>
         <div className="container">
             <p className="logo">마니마니또</p>
-            <TransitionGroup>
-                <CSSTransition key={key} timeout={500} classNames="fade" mountOnEnter unmountOnExit>
-                    <h1 className="greeting">{display}</h1>
-                </CSSTransition>
-            </TransitionGroup>
-
+            <div className="greeting">
+                {sentences[index].split('\n').map((line, index) => (
+                    <p key={index}>{line}</p>
+                ))}
+            </div>
             <div className="character-container">
                 <iframe src="https://lottie.host/embed/be36fba1-43f0-4d12-a882-cf40fa5d84bd/npTev2uCYy.json"></iframe>
             </div>
-            <button>마니또 방 만들기</button>
+            <button className="button1">마니또 방 만들기</button>
         </div>
         <style jsx>
             {`
@@ -45,20 +38,17 @@ export default function CreateHome() {
                 }
                 .greeting {
                     text-align: center;
+                    animation: fadeInOut 5s linear infinite;
                 }
-                .fade-enter {
-                    opacity: 0;
+
+                @keyframes fadeInOut {
+                    0% { opacity: 0; }
+                    50% { opacity: 1; }
+                    100% { opacity: 0; }
                 }
-                .fade-enter-active {
-                    opacity: 1;
-                    transition: opacity 1000ms;
-                }
-                .fade-exit {
-                    opacity: 1;
-                }
-                .fade-exit-active {
-                    opacity: 0;
-                    transition: opacity 1000ms;
+                
+                .greeting p{
+                    font-size: 32px;
                 }
 
                 .character-container {
@@ -87,16 +77,7 @@ export default function CreateHome() {
                     height: 100%;
 
                 }
-                
-                button{
-                    width: 300px;
-                    height: 70px;
-                    background-color: #00C5B3;
-                    color: white;
-                    border-radius: 60px;
-                    border: none;
-                    font-size: 23px;
-                }
+            
             `}
         </style>
     </>

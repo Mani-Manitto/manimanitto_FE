0016acf43174e39e194b68cfceff91050caff854
KECO-00 <ksang98@icloud.com>
2024-03-17 01:16:47 +0900

    chore: 랜딩 페이지 '/createManito/createMain' 으로 설정

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/createManito/createMain",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

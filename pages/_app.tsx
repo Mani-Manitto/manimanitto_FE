9ebcad0e9ab85599061cb22b6256536803c92784
KECO-00 <ksang98@icloud.com>
2024-04-16 16:35:15 +0900

    add: head 정보 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React, { useEffect } from 'react';
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
@@ -17,26 +18,32 @@ export default function App({ Component, pageProps }: AppProps) {
     };
   }, []);
   return (
-    <div className="container">
-      <Component {...pageProps} />
-      <style jsx global>{`
-        body {
-          margin: 0;
-          padding: 0;
-          background-color: #EEFFFB;
-          display: flex;
-          justify-content: center;
-        }
-        
-        .container {
-          min-width: 350px;
-          max-width: 500px;
-          width: 100vw;
-          height: calc(var(--vh, 1vh) * 100);
-          background-color: white;
-        }
-      `}</style>
-    </div>
+    <>
+      <Head>
+        <title>마니마니또</title>
+        <link rel="icon" href="/manimanitto.ico" />
+      </Head>
+      <div className="container">
+        <Component {...pageProps} />
+        <style jsx global>{`
+          body {
+            margin: 0;
+            padding: 0;
+            background-color: #EEFFFB;
+            display: flex;
+            justify-content: center;
+          }
+          
+          .container {
+            min-width: 350px;
+            max-width: 500px;
+            width: 100vw;
+            height: calc(var(--vh, 1vh) * 100);
+            background-color: white;
+          }
+        `}</style>
+      </div>
+    </>
     
   );
 }
